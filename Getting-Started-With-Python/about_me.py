622a59f030fcdf0bd09f299fd2fed6d8af6d4c2d
PPCocainee <109103685+PPCocainee@users.noreply.github.com>
2022-10-27 18:25:55 -0700

    Submitting Assignment 1

---
+++ Getting-Started-With-Python/about_me.py
@@ -0,0 +1,16 @@
+full_name = "Orlando Martinez"
+nickname = "flaco or junior"
+age = 17
+has_used_python = True
+
+hobbies = ["skating", "playing baseball", "playing video games", "watching anime", "hanging out wiht friends", "doing graffiti"]
+
+fav_things = {
+  "movie": "saw series",
+  "food": "hamburger",
+  "hobby": hobbies[4],
+  "anime": "bleach and darling and the franxx",
+  "number": 7
+}
+
+all_vars = dict(vars())
